3d88278630832dc58ba17c4ebb5c225d87eddebc
Kenya Francis <kenyarae99@gmail.com>
2024-08-28 14:20:37 +0000

    Collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -108,6 +108,22 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entires_sales():
+    """
+    Collects collumns of data from sales worksheet, collecting 
+    the last 5 entries for each sandwhich and return the data
+    as a list of lists
+    """
+    sales = SHEET.worksheet("sales")
+    # column = sales.col_values(3)
+    # print(column)
+    
+    columns = []
+    for ind in range(1, 7):
+       column = sales.col_values(ind)
+       columns.append(column[-5:])
+       pprint(columns)
+
 def main():
     """
     Run all program functions
@@ -119,4 +135,6 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwhiches Data Automation")
-main()
+# main()
+
+get_last_5_entires_sales()
