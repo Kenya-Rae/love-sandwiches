23a969d866f4783373110bf3244b95bf30250ac8
Kenya Francis <kenyarae99@gmail.com>
2024-08-28 12:58:48 +0000

    Add sales data to sales worksheet

--- run.py
+++ run.py
@@ -12,38 +12,40 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-def get_sales_date():
+def get_sales_data():
     """
-    Get Sales figures input from the user
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
-
     while True:
-        print("Please enter sales date from the last markert.")
-        print("Data should be six numbers, seperated by commas.")
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
 
         sales_data = data_str.split(",")
-        validate_date(sales_data)
 
-        if validate_date(sales_data):
-            print("Data is VALID!")
+        if validate_data(sales_data):
+            print("Data is valid!")
             break
 
-        return sales_data
+    return sales_data
 
-def validate_date(values):
+
+def validate_data(values):
     """
-    Inside the try, converts all string values into intergers.
-    Raise ValueError if strings cannot be converted into int,
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
     try:
-         [int(value) for value in values]
-         if len(values) != 6:
+        [int(value) for value in values]
+        if len(values) != 6:
             raise ValueError(
-                f"Exaclty 6 values are requried, you provided {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
@@ -52,4 +54,17 @@ def validate_date(values):
     return True
 
 
-data = get_sales_date()
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("Updating sales worksheet.../n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully./n")
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
